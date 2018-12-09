ee239dc2273f0667e95f9f7dce4f53745c2e9df2
Elissa Irhamy <elissairhamy@Elissas-MacBook-Pro-2.local>
2018-12-09 17:10:36 -0500

    Here are the codes for TasteDive API requests

---
+++ FP_recs.py
@@ -0,0 +1,88 @@
+# This is where TasteDive API function lives and checking with genre in TMDB API and ratings in rotten tomatoes
+
+## IMPORT ##
+from FP_cache import *
+from bs4 import BeautifulSoup
+from FP_secrets import *
+import requests
+from FP_codes import *
+
+## REQUEST FROM TASTEDIVE API ##
+def tastedive_rec(movie):
+    taste_base = "https://tastedive.com/api/similar"
+    taste_param = {"q": "movie:" + movie, "type": "movies", "k": taste_dive_key}
+    response = check_cache(taste_base, taste_param)
+    json_response = json.loads(response)
+    mov_rec_dict = json_response["Similar"]["Results"]
+    mov_names = []
+    for each in mov_rec_dict:
+        each_name = each.get("Name", "None")
+        mov_names.append(each_name)
+    #print(mov_names)
+    return mov_names
+
+#print(len(tastedive_rec("spiderman,inception")))
+
+## CHECK EACH MOVIE FROM TASTEDIVE WITH GENRE ID FROM TMDB, PUT IN DICTIONARY ##
+def recs_steps(movie, ori_movie_obj1, ori_movie_obj2):
+    tmdb_req = movie_dict(movie)
+    tmdb_mov_list = []
+    genre = tmdb_req[0].genre
+    #print("user movie: ", genre)
+    ori_genre1 = ori_movie_obj1.genre
+    #print("first movie genre: ", ori_genre1)
+    ori_genre2 = ori_movie_obj2.genre
+    #print("second movie genre: ", ori_genre2)
+    ori_genre = ori_genre1 + list(set(ori_genre2) - set(ori_genre1))
+    #print("combined list: ", ori_genre)
+    count = 0
+    same_genre_id = []
+    for each in genre:
+        if each in ori_genre:
+            same_genre_id.append(each)
+            count += 1
+    same_genre = []
+    for each in same_genre_id:
+        gen_id_name = genre_name(each)
+        same_genre.append(gen_id_name)
+    tmdb_mov_list.append(tmdb_req[0].title)
+    tmdb_mov_list.append(count)
+    tmdb_mov_list.append(same_genre)
+    #print(tmdb_mov_list)
+    return tmdb_mov_list
+
+## CHECK DICTIONARY WITH GENRE COUNT AND SORT THEM FROM MOST TO LEAST ACCORDING TO COUNT ##
+movie_genre_dict = {}
+def dict_genre_count(movie_genre_list):
+    # movie_genre_dict is a global dictionary
+    movie_genre_dict[movie_genre_list[0]] = [movie_genre_list[1], movie_genre_list[2]]
+    #print(movie_genre_dict)
+    return movie_genre_dict
+
+def sort_genre_count(dict):
+    genre_sorted = sorted(dict.items(), key = lambda x:x[1], reverse = True)
+    top_ten_list = []
+    count = 0
+    while count < 10:
+        top_ten_list.append(genre_sorted[count])
+        count += 1
+
+    #print top 10 movie name with their plots
+    for each in top_ten_list:
+        mov_info = movie_dict(each[0])
+        print(mov_info[0])
+        print("--------------------------")
+    return
+## TEST ##
+# test_ori_movie_dict1 = movie_dict("inception")
+# test_ori_movie_dict2 = movie_dict("the amazing spiderman")
+
+# test_recs = tastedive_rec("inception, the amazing spiderman")
+# for test_each in test_recs:
+#     test_genre_list = recs_steps(test_each, test_ori_movie_dict1[0], test_ori_movie_dict2[0])
+#     dict_genre_count(test_genre_list)
+# #print(len(movie_genre_dict.keys()))
+# sort_genre_count(movie_genre_dict)
+#list1 = recs_steps("shutter island",test_ori_movie_dict1[0],test_ori_movie_dict2[0])
+
+#print(movie_genre_dict)
