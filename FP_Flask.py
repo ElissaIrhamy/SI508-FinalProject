7c6a30566b4e8cc9ab6387329238625b2e0c34f9
Elissa Irhamy <elissairhamy@Elissas-MacBook-Pro-2.local>
2018-12-11 23:04:44 -0500

    This will be the finalized Flask code

--- FP_Flask.py
+++ FP_Flask.py
@@ -17,19 +17,8 @@ manager = Manager(app)
 
 @app.route('/', methods = ['GET'])
 def get_movie():
-    m = """
-<br><br><br><br><br>
-<h1><center><font face="helvetica">Welcome to Movie Recs!</h1></center>
-<h3><center>Please input two movies that you like below:</center></h3>
-<form action = 'http://localhost:5000/movieuser' method='GET'>
-<center>Movie 1:<br>
-<input type="text" name="mov1" value=""><br>
-Movie 2:<br>
-<input type="text" name="mov2" value=""><br>
-<input type="submit" value="Submit"></font></center>
-</form>
-    """
-    return m
+
+    return render_template('index.html')
 
 
 @app.route('/movieuser', methods = ['GET'])
@@ -51,7 +40,7 @@ def confirm_movie():
 
         m2_list = movie_dict(mov_two)
         if m2_list == None:
-            flash(mov_one + " is not a valid movie in our database :( Please try another movie!")
+            flash(mov_two + " is not a valid movie in our database :( Please try another movie!")
             return redirect(url_for('get_movie'))
         mov2_html = "<font face='helvetica'><ol>"
         for m2 in m2_list[0:10]:
@@ -59,10 +48,10 @@ def confirm_movie():
         mov2_html = mov2_html + "</ol>" + "<input type='submit' value='Submit'> </form></font>"
 
         m1_header = """
-<h2><font face="helvetica">Here are the movies I found. Which ones of these did you mean?</h2>
-<h3><i>(Choose one from the first list and one from the second list, then hit Submit at the bottom of the page)</i></font></h3>
+<h2 style='color:#5D6D7E'><font face="helvetica">Here are the movies I found. Which ones of these did you mean?</h2>
+<h4>(Choose one from the first list and one from the second list, then hit Submit at the bottom of the page)</font></h4>
         """
-        return "{}<br><h3><i>Your Movie 1 input: {}</i></h3>{}<br><h3><i>Your Movie 2 input: {}</i></h3>{}".format(m1_header, mov_one, mov1_html, mov_two, mov2_html)
+        return "{}<h3><i>Your Movie 1 input: {}</i></h3>{}<br><h3><i>Your Movie 2 input: {}</i></h3>{}".format(m1_header, mov_one, mov1_html, mov_two, mov2_html)
 
 @app.route('/chosenmovies', methods = ['GET'])
 def movie_chosen():
@@ -85,7 +74,7 @@ def movie_chosen():
             #print(rec_info)
             rec_mov = rec_mov + "<li>" + rec_info[0].__str__() + "</li>"
         rec_mov = rec_mov + "</font></ul>"
-        return "<font face='helvetica'>The movies you chose are: {} and {}.<br> <h3>Based on the movies you chose, you might also like these movies. ENJOY!</font></h3>{}".format(movie_one,movie_two,rec_mov)
+        return "<font face='helvetica'><i>The movies you chose are: {} and {}.</i><br> <h3 style='color:#5D6D7E'>Based on the movies you chose, you might also like these movies. ENJOY!</font></h3>{}".format(movie_one,movie_two,rec_mov)
 
 if __name__ == '__main__':
     manager.run() # Runs the flask server in a special way that makes it nice to debug
