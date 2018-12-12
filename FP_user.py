d851457fcecaac1d03af95de539cca7c0e59f778
Elissa Irhamy <elissairhamy@Elissas-MacBook-Pro-2.local>
2018-12-11 23:15:58 -0500

    Use this only to test codes in terminal. otherwise, not needed

--- FP_user.py
+++ FP_user.py
@@ -7,13 +7,13 @@ from FP_codes import *
 from FP_recs import *
 
 # ASK FOR MOVIE #
-# print("HELLO! Welcome to my movie rec!")
-# user_in = input("Please input two movies you like separated by a comma(,) -  ")
+print("HELLO! Welcome to my movie rec!")
+user_in = input("Please input two movies you like separated by a comma(,) -  ")
 #
 # # PRODUCE LIST #
-# user_in_list = user_in.split(",")
-# user_in_one = user_in_list[0]
-# user_in_two = user_in_list[1]
+user_in_list = user_in.split(",")
+user_in_one = user_in_list[0]
+user_in_two = user_in_list[1]
 
 def produce_list(user, m_num):
     obj_list = movie_dict(user)
@@ -25,9 +25,9 @@ def produce_list(user, m_num):
         print("---------------------------\n")
         count += 1
     # CHOOSE MOVIE #
-    # user_num = input("Choose the number next to the " + m_num +" movie that you would like recommendations for - ")
-    # user_chosen = obj_list[int(user_num) - 1].title
-    return
+    user_num = input("Choose the number next to the " + m_num +" movie that you would like recommendations for - ")
+    user_chosen = obj_list[int(user_num) - 1].title
+    return user_chosen
 # movie_confirm = input("Is this the correct movie? [Y/N] - ")
 # if movie_confirm.upper() == "N":
 #     user_num = input("Choose the correct number - ")
@@ -36,17 +36,19 @@ def produce_list(user, m_num):
 # rotten_title = cleaned_title(user_chosen_movie)
 # print(dvd_stream_movie(rotten_title))
 
-# mov_one = produce_list(user_in_one, "1st")
-# mov_two = produce_list(user_in_two, "2nd")
-# mov_one_obj = movie_dict(mov_one)
-# mov_two_obj = movie_dict(mov_two)
-# #print([mov_one, mov_two])
-# #movie_genre_dict = {}
-# print("---------------------------------------------------------------")
-# print("Here are the movies similar to the two movies you chose. ENJOY!")
-# print("---------------------------------------------------------------")
-# recs = tastedive_rec(mov_one+","+mov_two)
-# for each in recs:
-#     genre_list = recs_steps(each, mov_one_obj[0], mov_two_obj[0])
-#     dict_genre_count(genre_list)
-# sort_genre_count(movie_genre_dict)
+mov_one = produce_list(user_in_one, "1st")
+#print(mov_one)
+mov_two = produce_list(user_in_two, "2nd")
+mov_one_obj = movie_dict(mov_one)
+#print(mov_one_obj)
+mov_two_obj = movie_dict(mov_two)
+#print([mov_one, mov_two])
+#movie_genre_dict = {}
+print("---------------------------------------------------------------")
+print("Here are the movies similar to the two movies you chose. ENJOY!")
+print("---------------------------------------------------------------")
+recs = tastedive_rec(mov_one+","+mov_two)
+for each in recs:
+    genre_list = recs_steps(each, mov_one_obj[0], mov_two_obj[0])
+    dict_genre_count(genre_list)
+sort_genre_count(movie_genre_dict)
