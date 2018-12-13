270b17e456465cf1ec8cbb07e7b853e7d8c7581f
Elissa Irhamy <elissairhamy@Elissas-MacBook-Pro-2.local>
2018-12-13 18:38:26 -0500

    Added bonus section where bar chart of budget and revenue appears when chosen and added a final Thank You page

--- FP_Flask.py
+++ FP_Flask.py
@@ -4,7 +4,6 @@ from flask_script import Manager
 import requests
 from FP_codes import *
 from FP_recs import *
-#from FP_user import *
 
 
 # Set up application
@@ -27,13 +26,16 @@ def confirm_movie():
         result = request.args
         mov_one = result.get("mov1")
         mov_two = result.get("mov2")
+        # for error
         if mov_one == "" or mov_two == "":
             flash("Please input TWO movies")
             return redirect(url_for('get_movie'))
+        # for error
         if mov_one == "" and mov_two =="":
             flash("Please input TWO movies")
             return redirect(url_for('get_movie'))
         m1_list = movie_dict(mov_one)
+        # for error
         if m1_list == None:
             flash(mov_one + " is not a valid movie in our database :( Please try another movie!")
             return redirect(url_for('get_movie'))
@@ -45,6 +47,7 @@ def confirm_movie():
         mov1_html = mov1_html + "</font></ol>"
 
         m2_list = movie_dict(mov_two)
+        # for error
         if m2_list == None:
             flash(mov_two + " is not a valid movie in our database :( Please try another movie!")
             return redirect(url_for('get_movie'))
@@ -56,7 +59,7 @@ def confirm_movie():
         mov2_html = mov2_html + "</ol>" + "<input type='submit' value='Submit'> </form></font>"
 
         m1_header = """
-<h2 style='color:#5D6D7E'><font face="helvetica">Here are the movies I found. Which ones of these did you mean?</h2>
+<br><br><h2 style='color:#5D6D7E'><font face="helvetica">Here are the movies I found. Which ones of these did you mean?</h2>
 <h4>(Choose one from the first list and one from the second list, then hit Submit at the bottom of the page)</font></h4>
         """
         #m1_error = """ <h4 style='color:#7B241C; background-color:#F4F6F7'> Make sure you CLICK TWO BUTTONS or the page WILL break :( </h4>"""
@@ -64,6 +67,7 @@ def confirm_movie():
 
 @app.route('/chosenmovies', methods = ['GET'])
 def movie_chosen():
+
     if request.method == "GET":
         results = request.args
         #print(results)
@@ -89,7 +93,28 @@ def movie_chosen():
             #print(rec_info)
             rec_mov = rec_mov + "<li>" + rec_info[0].__str__() + "</li>"
         rec_mov = rec_mov + "</font></ul>"
-        return "<font face='helvetica'><i>The movies you chose are: {} and {}.</i><br> <h3 style='color:#5D6D7E'>Based on the movies you chose, you might also like these movies. ENJOY!</font></h3>{}".format(movie_one,movie_two,rec_mov)
+        p = """Would you like to know if these movies made a lot of money?
+        <form action='http://localhost:5000/numplot'method='GET'>
+        <font face='helvetica'>
+        <input type='radio' name='mov' value=""" + movie_one + "> " + movie_one + '<br>' + "<input type='radio' name='mov' value=" + movie_two + "> " + movie_two + "<br><input type='submit' value='Show Me!'></form></font>"
+
+        return "<br><font face='helvetica'><i>The movies you chose are: {} and {}.</i><br> <h3 style='color:#5D6D7E'>Based on the movies you chose, you might also like these movies. ENJOY!</font></h3>{}<center><h3 style='color:#7B241C; background-color:#F4F6F7'> Bonus </h3></center>{}".format(movie_one,movie_two,rec_mov,p)
+
+@app.route('/numplot', methods = ['GET'])
+def num_chart():
+    if request.method == "GET":
+        results = request.args
+        mov_num = results.get('mov')
+        mov_id = movie_dict(mov_num)[0].id
+        mov_id_req = request_movie_num(mov_id)
+        mov_num_obj = MovieNumbers(mov_id_req)
+        budget = mov_num_obj.budget
+        revenue = mov_num_obj.revenue
+        #print(budget, revenue)
+        if budget == 0 or revenue == 0:
+            return "<center> Sorry, we don't have that data.<h2>BUT THANK YOU FOR USING MY APPLICATION!</h2></center>"
+        plot_numbers(budget,revenue)
+        return "<center><h1>THANKS FOR USING MY APPLICATION!!</center></h1>"
 
 if __name__ == '__main__':
     manager.run() # Runs the flask server in a special way that makes it nice to debug
